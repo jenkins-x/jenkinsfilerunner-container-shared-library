e57bcbbbf38f0a7685cc8a2fe0a6adb3a5ee8120
James Strachan <james.strachan@gmail.com>
2018-09-04 05:51:32 +0100

    try add warning for different container names used

--- vars/container.groovy
+++ vars/container.groovy
@@ -1,5 +1,7 @@
 #!/usr/bin/groovy
 
+def globalContainerName = ""
+
 /** simulates the container(name) { ... } expression in kubernetes-plugin */
 def call(String name = "", body) {
     // evaluate the body block, and collect configuration into the object
@@ -8,6 +10,14 @@ def call(String name = "", body) {
     body.delegate = config
 
     echo "invoking container ${name}"
+
+    if (globalContainerName == "") {
+        globalContainerName = name
+    } else {
+        if (globalContainerName != name) {
+            echo "WARNING: multiple container images are not yet supported in 'container(x) { ... }' blocks! So cannot run command in conatiner ${name} so using ${globalContainerName} instead!"
+        }
+    }
     
     body()
 }
