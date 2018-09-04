a9106104f22453096fc4d7ce0be7f431a38aa4bc
James Strachan <james.strachan@gmail.com>
2018-09-04 05:56:29 +0100

    avoid global variable - use system property

--- vars/container.groovy
+++ vars/container.groovy
@@ -1,7 +1,5 @@
 #!/usr/bin/groovy
 
-def globalContainerName = ""
-
 /** simulates the container(name) { ... } expression in kubernetes-plugin */
 def call(String name = "", body) {
     // evaluate the body block, and collect configuration into the object
@@ -11,8 +9,10 @@ def call(String name = "", body) {
 
     echo "invoking container ${name}"
 
-    if (globalContainerName == "") {
-        globalContainerName = name
+    def key = "io.jenkins.jenkinsfile-runner.container"
+    def globalContainerName = System.getProperty(key)
+    if (!globalContainerName) {
+        System.setProperty(key, name)
     } else {
         if (globalContainerName != name) {
             echo "WARNING: multiple container images are not yet supported in 'container(x) { ... }' blocks! So cannot run command in conatiner ${name} so using ${globalContainerName} instead!"
